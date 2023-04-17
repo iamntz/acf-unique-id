41a0b2dfdf57b69da33259c2976d56d3966875ba
Ionuț Staicu <github@iamntz.com>
2023-04-17 11:23:51 +0300

    fix names

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <?php
-
-use iamntz\acf\unique_id\AcfUniqueID;
-
-!defined('ABSPATH') && die();
-
 /*
 Plugin Name: ACF Unique ID Field
 Author: Ionuț Staicu
@@ -11,16 +6,21 @@ Version: 1.0.0
 Author URI: https://ionutstaicu.com
 */
 
+use iamntz\acf\unique_id\AcfUniqueID;
+
+!defined('ABSPATH') && die();
+
+
 if (!defined('ABSPATH')) {
     exit;
 }
 
-define('ACF_UN_VERSION', '1.0.0');
+define('NTZ_ACF_UNIQUE_ID_VERSION', '1.0.0');
 
-define('ACF_UN_BASEFILE', __FILE__);
-define('ACF_UN_URL', plugin_dir_url(__FILE__));
-define('ACF_UN_PATH', plugin_dir_path(__FILE__));
-define('ACF_UN_RELATIVE_PATH', dirname(plugin_basename(__FILE__)));
+define('NTZ_ACF_UNIQUE_ID_BASEFILE', __FILE__);
+define('NTZ_ACF_UNIQUE_ID_URL', plugin_dir_url(__FILE__));
+define('NTZ_ACF_UNIQUE_ID_PATH', plugin_dir_path(__FILE__));
+define('NTZ_ACF_UNIQUE_ID_RELATIVE_PATH', dirname(plugin_basename(__FILE__)));
 
 
 add_action('acf/include_field_types', function () {
